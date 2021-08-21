e6520455393b47a32ffde8ba430d3674b9cdf63f
kosuke-shibata <83070201+kosuke-shibata@users.noreply.github.com>
2021-08-22 07:28:22 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,28 @@
+$(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 650) {
+      $('.bg-black').addClass('fixed');
+    } else {
+      $('.bg-black').removeClass('fixed');
+    }
+  });
+});
+
+$(function() {
+  let pageTop = $('.page-top');
+  pageTop.hide();
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      pageTop.fadeIn();
+    } else {
+      pageTop.fadeOut();
+    }
+  });
+  pageTop.click(function() {
+    $('body, html').animation({
+      scrollTop: 0
+    }, 500);
+    return false;
+  });
+});
